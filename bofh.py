0de22bdd7b4d050d0b7f720fd2833ea689eff2e8
Ty Brown <me@tybrown.net>
2016-11-27 13:26:49 -0600

    Better path joining

--- bofh.py
+++ bofh.py
@@ -6,6 +6,6 @@ import os
 
 bofh_dir = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
 
-with open(bofh_dir + os.path.sep + "bofh.conf", "r") as f:
+with open(os.path.join(os.path.sep, bofh_dir, "bofh.conf"), "r") as f:
     lines = [line.strip() for line in f.readlines()]
 print "BOFH Excuse of the Day: \t%s" % random.choice(lines)
